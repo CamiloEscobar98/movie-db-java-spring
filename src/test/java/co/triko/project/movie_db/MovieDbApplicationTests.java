0d08d53c1aba2031e5cdf546b113bcb3d0cd806b
Andrés Yáñez <camilo_escobar2398@outlook.com>
2024-12-21 22:58:15 -0500

    Iniciando proyecto Movie-DB Java Spring Boot

---
+++ src/test/java/co/triko/project/movie_db/MovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package co.triko.project.movie_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
