5108063f43ff864c694f89abccad849e63939a84
Andrés Yáñez <camilo_escobar2398@outlook.com>
2024-12-21 23:45:32 -0500

    Testeando la aplicación, retomando dependencias de Maven y Plugins. Corriendo por primera vez la aplicación.

---
+++ src/main/java/co/triko/project/movie_db/api/controllers/TestController.java
@@ -0,0 +1,13 @@
+package co.triko.project.movie_db.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World 1";
+    }
+}
